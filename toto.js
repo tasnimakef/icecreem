81eb93693d61063abedd480f521e496c0bd714a0
tasnimakef <tasneemalzoaby1989@gmail.com>
2021-01-27 17:33:37 +0200

    new folder

---
+++ toto.js
@@ -0,0 +1,23 @@
+alert('123')
+console.log(1234);
+console.log('tasnem');
+confirm("press");
+
+var time = new Date() . getHours();
+
+if (time<10)
+{ alert ('good morning');
+}
+else if (time < 20){
+    alert ('good day');
+} else {
+    alert ('good evening');
+}
+
+
+
+
+
+
+
+
